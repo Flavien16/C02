f8baf7503fa043f337a239d71ebfb42422d3fba9
Flavien Camoin <fcamoin@z3r8p3.42lyon.fr>
2023-07-12 16:02:43 +0200

    ex10

--- ex10/ft_strlcpy.c
+++ ex10/ft_strlcpy.c
@@ -6,10 +6,28 @@
 /*   By: fcamoin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/12 13:42:21 by fcamoin           #+#    #+#             */
-/*   Updated: 2023/07/12 13:55:54 by fcamoin          ###   ########.fr       */
+/*   Updated: 2023/07/12 16:00:06 by fcamoin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 unsigned int	ft_strlcpy(char *dest, char *src, unsigned int size)
 {
+	unsigned int src_len = 0;
+	unsigned int i = 0;
+
+	while(src[src_len] != '\0')
+	{
+		src_len++;
+	}
+
+	if (size > 0)
+	{
+		while (src[i] != '\0' && i < (size - 1))
+		{
+			dest[i] = src[i];
+			i++;
+		}
+		dest[i] = '\0';
+	}
+	return (src_len);
 }
